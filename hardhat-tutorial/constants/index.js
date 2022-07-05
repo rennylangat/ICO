d2c983ac750a4c00a985d439b8b49191a8b3d17a
Renny Langat <rennylngt@gmail.com>
2022-07-05 17:04:31 +0300

    First commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xBb4D90D122cBF80a6f441c8613ED53E5f0241DB6";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
---
+++ hardhat-tutorial/contracts/CryptoDevToken.sol
@@ -0,0 +1,107 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoDevs.sol";
+
+contract CryptoDevToken is ERC20, Ownable {
+    //Price of one CryptoDevToken
+
+    uint256 public constant tokenPrice = 0.001 ether;
+
+    // Each NFT would give the user 10 tokens
+    // It needs to be represented as 10 * (10 ** 18) as ERC20 tokens are represented by the smallest denomination possible for the token
+    // By default, ERC20 tokens have the smallest denomination of 10^(-18). This means, having a balance of (1)
+    // is actually equal to (10 ^ -18) tokens.
+    // Owning 1 full token is equivalent to owning (10^18) tokens when you account for the decimal places.
+    // More information on this can be found in the Freshman Track Cryptocurrency tutorial.
+
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+
+    uint256 public constant maxTotalSupply = 1000 * 10**18;
+
+    ICryptoDevs CryptoDevsNFT;
+
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(address _cryptoDevsContract) ERC20("CryptoDev Token", "CD") {
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
+    }
+
+    /**
+     * @dev Mints `amount` number of CryptoDevTokens
+     *Requirements:
+     *-`msg.value` should be equal or greater than the tokenPrice * amount
+     */
+
+    function mint(uint256 amount) public payable {
+        //value of ehter that should be equal or greater than tokenPrice * amount
+        uint256 _requiredAmount = tokenPrice * amount;
+
+        require(msg.value >= _requiredAmount, "Ether sent is incorrect");
+
+        //totaltokens + amount <=10000. otherwise revert the transaction
+
+        uint256 amountWithDecimals = amount * 10**18;
+
+        require(
+            (totalSupply() + amountWithDecimals) <= maxTotalSupply,
+            "Exceeds the max supply available"
+        );
+
+        _mint(msg.sender, amountWithDecimals);
+    }
+
+    /**
+     * @dev Mints tokens based on the number of NFT's held by the sender
+     * Requirements:
+     * balance of Crypto Dev NFT's owned by the sender should be greater than 0
+     * Tokens should have not been claimed for all the NFTs owned by the sender
+     */
+
+    function claim() public {
+        address sender = msg.sender;
+
+        //Get the number of CryptoDev NFTs held by a given sender address
+
+        uint256 balance = CryptoDevsNFT.balanceOf(sender);
+
+        require(
+            balance > 0,
+            "You do not own any CryptoDev NFTs buana. Umesota"
+        );
+
+        uint256 amount = 0;
+
+        for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+            if (!tokenIdsClaimed[tokenId]) {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+
+        require(amount > 0, "You have already claimed all the tokens");
+
+        _mint(msg.sender, amount * tokensPerNFT);
+    }
+
+    /**
+     * @dev withdraws all ETH and tokens sent to the contract
+     * Requirements:
+     * wallet connected must be owner's address
+     */
+
+     function withdraw() public onlyOwner{
+        address _owner=owner();
+        uint256 amount = address(this).balance;
+        (bool sent,)=_owner.call{value:amount}("");
+        require(sent,"Failed to send Ether");
+     }
+
+     receive() external payable{}
+
+     fallback() external payable {}
+}
